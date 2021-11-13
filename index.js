27f1ed3fd9f847b7aa40c3f81a01acc5e676c0c4
miltonbolonha <miltonbolonha@gmail.com>
2021-11-13 19:13:00 -0300

    v.0.1.5.bugfix/layout-export

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
   Export with named export to be able to pull in from library
 */
 
-export { Layout } from './src/nu_modules/layout'
+import React from 'react'
+import AgentResolver from './src/nu_modules/layout/containers'
+
+const Layout = ({ type, opt, children }) => {
+	return <AgentResolver type={type} opt={opt} subAgent={children} />
+}
+
+export default Layout
