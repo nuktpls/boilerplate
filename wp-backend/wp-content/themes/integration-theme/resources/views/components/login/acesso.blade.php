9c1c0e6f70a271885334388b1b0edd936c2ad42a
miltonbolonha <miltonbolonha@gmail.com>
2021-12-11 14:16:51 -0300

    major changes

--- wp-backend/wp-content/themes/integration-theme/resources/views/components/login/acesso.blade.php
+++ wp-backend/wp-content/themes/integration-theme/resources/views/components/login/acesso.blade.php
@@ -14,7 +14,19 @@
 </p> {{-- Descrição --}}
 
 <form id="login-form-ajax" class="login__access-form modal-content border-0" method="post">
-  @include( 'components.login.form' )
+	<div class="form-group position-relative mb-4">
+    <i class="login__access-form-icons position-absolute text-primary uil uil-at"></i>
+    <input id="email-input" class="form-control border-0 border-bottom rounded-0" type="text" placeholder="Digite seu e-mail" name="email" required>
+  </div>
+  <div class="form-group position-relative mb-4">
+    <i class="login__access-form-icons position-absolute text-primary uil uil-lock"></i>
+    <input id="password-input" class="form-control border-0 border-bottom rounded-0" type="password" placeholder="Digite sua senha" name="password" required>
+  </div>
+  <div class="d-grid gap-2">
+    <button id="wp-submit" class="btn btn-primary fs-4" type="submit"
+    disabled="true"
+    >Acessar</button>
+  </div>
 </form> {{-- Formulário --}}
 <div id="message-error" class="alert alert-danger" role="alert" style="display:none">
   This is a danger alert—check it out!
