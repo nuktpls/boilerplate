b77e7f131f3fcccdd8ce235120b1ad6fad906fe2
miltonbolonha <miltonbolonha@gmail.com>
2021-12-10 21:39:34 -0300

    major changes

--- wp-backend/wp-content/themes/integration-theme/dist/scripts/main.js
+++ wp-backend/wp-content/themes/integration-theme/dist/scripts/main.js
@@ -22080,27 +22080,27 @@ var configLogin = {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 var configSignupRequest = {
-  configTo: "mbolonha@studiovisual.com.br",
-  configSubject: "Pedido de Cadastro",
-  configBody: "Email enviado por meio do site",
-  configHeadersFrom: "From: Número 1 Seguros Teste <mbolonha@studiovisual.com.br>",
-  configHeadersCC: "Cc: The Bolonha <miltonbolonha@gmail.com>",
-  ajax_url: "/wp-admin/admin-ajax.php",
-  redirect: "/",
-  errorElement: "modal-message-error-nope",
-  successElement: "message-success",
+  configTo: 'mbolonha@deletar.com.br',
+  configSubject: 'Pedido de Cadastro',
+  configBody: 'Email enviado por meio do site',
+  configHeadersFrom: 'From: Número 1 Seguros Teste <mbolonha@deletar.com.br>',
+  configHeadersCC: 'Cc: The Bolonha <miltonbolonha@gmail.com>',
+  ajax_url: '/wp-admin/admin-ajax.php',
+  redirect: '/',
+  errorElement: 'modal-message-error-nope',
+  successElement: 'message-success',
   successMsg: "<h2 style=\"font-size:24px;color:#3CB195\">Mensagem enviada com sucesso!</h2>\n  <p style=\"font-size:14px; color:#58595B\">\n    Geralmente nossos consultores demoram cerca de <strong>4 \xE0 8h</strong> para entrar em contato por e-mail ou telefone.\n  </p>\n  <p style=\"font-size:14px; color:#58595B\">\n  <strong>Qualquer d\xFAvida, fale conosco atrav\xE9s do nosso canal via <span style=\"color:#EBBA1F\">WhatsApp</span></strong>\n  </p>",
-  errorMsg: "Autorização negada, verifique as suas credenciais e tente novamente.",
-  action: "signup_request",
-  submitFormBtn: "sign-up-btn",
-  form: "sign-up-form",
-  contactNameId: "nomeCompleto",
-  phoneNumberId: "telNo",
-  businessEmailId: "emailEmpresa",
-  businessNameId: "nomeEmpresa",
-  businessCountryRegisterId: "dadosEmpresa",
-  contactMessageId: "mensagemEmpresa",
-  inputsId: "nomeCompleto telNo emailEmpresa nomeEmpresa dadosEmpresa mensagemEmpresa"
+  errorMsg: 'Autorização negada, verifique as suas credenciais e tente novamente.',
+  action: 'signup_request',
+  submitFormBtn: 'sign-up-btn',
+  form: 'sign-up-form',
+  contactNameId: 'nomeCompleto',
+  phoneNumberId: 'telNo',
+  businessEmailId: 'emailEmpresa',
+  businessNameId: 'nomeEmpresa',
+  businessCountryRegisterId: 'dadosEmpresa',
+  contactMessageId: 'mensagemEmpresa',
+  inputsId: 'nomeCompleto telNo emailEmpresa nomeEmpresa dadosEmpresa mensagemEmpresa'
 };
 /* harmony default export */ __webpack_exports__["default"] = (configSignupRequest);
 
