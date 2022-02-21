e3b2023c66b74eccbf13e8ae6057eb71849ad9aa
miltonbolonha <miltonbolonha@gmail.com>
2022-02-21 14:02:15 -0300

    bgimage added

--- src/nu_modules/layout/components/Body.js
+++ src/nu_modules/layout/components/Body.js
@@ -1,4 +1,14 @@
+// eslint-disable-next-line
 import React from 'react'
 
-const Body = ({ children }) => <>{children}</>
+const Body = ({ children, customClasses, bgImage }) => (
+  <div
+    className={`atomic-body ${customClasses}`}
+    style={{
+      backgroundImage: `url(${bgImage.src})`,
+    }}
+  >
+    {children}
+  </div>
+)
 export default Body
