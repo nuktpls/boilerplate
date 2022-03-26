7a1233433abf4a0a42452fc4eb7ce3c6af8b9d5a
miltonbolonha <miltonbolonha@gmail.com>
2022-03-26 18:47:12 -0300

    feature/v.0.4.0 alpha v

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import './src/scss/styles.scss'
 import { GlobalProvider } from './src/nu_modules/layout/services/context'
 export const wrapRootElement = ({ element }) => (
-	<GlobalProvider>{element}</GlobalProvider>
+  <GlobalProvider>{element}</GlobalProvider>
 )
