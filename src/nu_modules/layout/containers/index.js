d4cc9b8a273da386c744f607d069e05826953d5d
miltonbolonha <miltonbolonha@gmail.com>
2022-03-26 15:19:28 -0300

    bugfix/v3.0.2 subAgent children var

--- src/nu_modules/layout/containers/index.js
+++ src/nu_modules/layout/containers/index.js
@@ -7,40 +7,40 @@ import BlockImageContainer from './BlockImageContainer'
 import FormContainer from './FormContainer'
 
 const LayoutResolver = ({
-	children,
-	opt,
-	type,
-	sectionTitle,
-	setLocation,
-	logo,
+  subAgent,
+  opt,
+  type,
+  sectionTitle,
+  setLocation,
+  logo,
 }) => {
-	function renderComponent(renderThis) {
-		switch (renderThis) {
-			case 'BODY':
-				return <BodyContainer children={children} opt={opt} />
-			case 'FOOTER':
-				return <FooterContainer children={children} opt={opt} />
-			case 'BLOCK_IMAGE':
-				return <BlockImageContainer opt={opt} />
-			case 'SEARCH':
-				return <FormContainer opt={opt} />
-			// case 'BLOCK_SHARE':
-			// return <BlockShareContainer opt={opt} />
-			case 'HEADER':
-				return <HeaderContainer logo={logo} opt={opt} />
-			case 'ROW':
-				return (
-					<RowContainer
-						opt={opt}
-						children={children}
-						sectionTitle={sectionTitle}
-						setLocation={setLocation}
-					/>
-				)
-			default:
-				return console.log(renderThis)
-		}
-	}
-	return <>{renderComponent(type)}</>
+  function renderComponent(renderThis) {
+    switch (renderThis) {
+      case 'BODY':
+        return <BodyContainer children={subAgent} opt={opt} />
+      case 'FOOTER':
+        return <FooterContainer children={subAgent} opt={opt} />
+      case 'BLOCK_IMAGE':
+        return <BlockImageContainer opt={opt} />
+      case 'SEARCH':
+        return <FormContainer opt={opt} />
+      // case 'BLOCK_SHARE':
+      // return <BlockShareContainer opt={opt} />
+      case 'HEADER':
+        return <HeaderContainer logo={logo} opt={opt} />
+      case 'ROW':
+        return (
+          <RowContainer
+            opt={opt}
+            children={subAgent}
+            sectionTitle={sectionTitle}
+            setLocation={setLocation}
+          />
+        )
+      default:
+        return console.log(renderThis)
+    }
+  }
+  return <>{renderComponent(type)}</>
 }
 export default LayoutResolver
