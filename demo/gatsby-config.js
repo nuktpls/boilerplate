1d9be3840e905c0175c17fc67e7a48669e56b607
miltonbolonha <miltonbolonha@gmail.com>
2022-04-06 21:08:23 -0300

    v.0.6.1 bundle analizer

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,5 +3,11 @@ module.exports = {
     {
       resolve: 'gatsby-layout-builder',
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+      options: {
+        devMode: true,
+      },
+    },
   ],
 }
